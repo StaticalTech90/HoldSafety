213fcea7d6f4ef215bcb71c16ff9a9ef8c9d8875
FRDFBL <37649179+StaticalTech90@users.noreply.github.com>
2021-08-17 20:00:52 +0800

    added registration function [WIP]

--- HoldSafety/app/src/main/java/com/example/holdsafety/Register2Activity.java
+++ HoldSafety/app/src/main/java/com/example/holdsafety/Register2Activity.java
@@ -1,21 +1,38 @@
 package com.example.holdsafety;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Register2Activity extends AppCompatActivity {
     public Uri imageURI;
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void uploadID(View view){
@@ -23,11 +40,30 @@ public class Register2Activity extends AppCompatActivity {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, 1);
+        //startActivityForResult(intent, 1); this is deprecated
     }
 
     public void userRegister(View view){
-        Toast.makeText(getApplicationContext(), "Register", Toast.LENGTH_SHORT).show();
+        //get values from register1Activity
+        Intent intent = getIntent();
+        CollectionReference usersCollectionRef = db.collection("users");
+
+        //register
+        mAuth.createUserWithEmailAndPassword("email","password").addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "signUpWithEmailPassword:success");
+                FirebaseUser user = mAuth.getCurrentUser();
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "signUpWithEmailPassword:failure", task.getException());
+                Toast.makeText(getApplicationContext(), "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //nani
+        //Toast.makeText(getApplicationContext(), "Register", Toast.LENGTH_SHORT).show();
     }
 
     @Override
