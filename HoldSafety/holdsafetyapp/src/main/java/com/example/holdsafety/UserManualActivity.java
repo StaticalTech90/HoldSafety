a8a79cc895e0c6dfbeaa08cbe021386cab0062c9
NCIcaro <nhoriza008@gmail.com>
2022-03-14 00:47:39 +0800

    bug fixes: add contact validation, mobile number char limit

--- HoldSafety/holdsafetyapp/src/main/java/com/example/holdsafety/UserManualActivity.java
+++ HoldSafety/holdsafetyapp/src/main/java/com/example/holdsafety/UserManualActivity.java
@@ -14,12 +14,6 @@ public class UserManualActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_manual);
-        String pdf = getIntent().getStringExtra("pdf_url");
-
-        WebView userManualWebview = findViewById(R.id.userManualWebview);
-
-        userManualWebview.setWebViewClient(new WebViewClient());
-        userManualWebview.loadUrl("https://docs.google.com/gview?embedded=true&url=" + pdf);
 
         ImageView btnBack = findViewById(R.id.backArrow);
         btnBack.setOnClickListener(view -> goBack());
